4ee648e3d230cb8deda8992cfcd4929ffe2434a2
JPStroud <jpstroud@gmail.com>
2021-12-11 19:47:20 -0500

    add: icons

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ const appTitle = "GASr Bootstrap5 Template";
 
 function doGet(e) {
   let html = HtmlService.createTemplateFromFile("page/html");
-  return html.evaluate().setTitle(appTitle);
+  return html.evaluate().setTitle(appTitle)
 };
