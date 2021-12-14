0a4ace7684fa9186f352f7fdeb24b881890febea
JPStroud <jpstroud@gmail.com>
2021-12-13 19:53:58 -0500

    init Bootstrap starter template

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,9 @@ const appTitle = "GASr Bootstrap5 Template";
 
 function doGet(e) {
   let html = HtmlService.createTemplateFromFile("page/html");
-  return html.evaluate().setTitle(appTitle)
-};
+  return html
+    .evaluate()
+    .setTitle(appTitle)
+    .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+    .addMetaTag("viewport", "width=device-width, initial-scale=1");
+}
